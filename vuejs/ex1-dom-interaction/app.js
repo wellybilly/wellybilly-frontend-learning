6ceb8060039040a78931f5364438405e50cf3252
billwangnz2015 <bill.wang.nz@outlook.com>
2020-11-15 16:09:27 +1300

    init event binding

--- vuejs/ex1-dom-interaction/app.js
+++ vuejs/ex1-dom-interaction/app.js
@@ -5,6 +5,17 @@ const app = Vue.createApp({
       courseGoal: 'Finish the course and learn Vue!',
       vueLink: 'https://vuejs.org/'
     }; 
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5){
+        return 'Learn Vue!';
+      }
+      else{
+        return 'Master Vue!';
+      }
+    }
   }
 });
 
